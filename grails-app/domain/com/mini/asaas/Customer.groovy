4139b8cd2bfeabdab88337276b98cc6ec16f1f8c
Brenda Oliveira <1brendabarbosa@gmail.com>
2025-06-11 10:15:42 -0300

    refactor: atributos renomeados em inglês e apenas declaração de constrains nullable

--- grails-app/domain/com/mini/asaas/Customer.groovy
+++ grails-app/domain/com/mini/asaas/Customer.groovy
@@ -1,20 +1,21 @@
 package com.mini.asaas
 import com.mini.asaas.utils.BaseEntity
 
-class Customer extends BaseEntity{
-   String nome
+class Customer extends BaseEntity {
+   String name
    String email
    Integer cpfCnpj
-   Integer cep
-   String logradouro
-   String numero 
+   Integer postalCode
+   String adress
+   String adressNumber 
+
    static constraints = {
-        nome blank: false, maxSize: 255
-        email email: true, blank: false, maxSize:255
-        cpfCnpj blank: false, nullable: false, size:11..14 
-        cep minSize: 8, maxSize: 8, blank: false, nullable: false
-        logradouro blank: false, nullable: false, maxSize: 255
-        numero blank: false, nullable: false, maxSize: 255
+        name blank: false
+        email email: true, nullable: false
+        cpfCnpj nullable: false
+        postalCode nullable: false
+        adress nullable: false
+        adressNumber nullable: false
     }
    
 }
